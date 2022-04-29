55aa5cd4910039f834d5965c515ce5bee988806c
Thaty Francelino <thatianefrancelinor@gmail.com>
2022-04-28 22:03:58 -0300

    codigo de aula coim comentários

---
+++ codigo-da-aula.js
@@ -0,0 +1,58 @@
+//Resolvendo problema de condicional - retomando a aula passada
+//let pessoa = {
+//'nome': "Tiago Sestari",
+   // 'idade': 30,
+    //'temIngresso': true
+//}
+
+//pessoa. idade é a mesma coisa que pessoa['idade']
+//vamos um console para ver como acessar nosso dado
+//console. log(pessoa['nome'])
+
+
+//dica:&& - E || - ou
+//if (pessoa.idade >= 18 && pessoa.temIngresso == true) {
+    //console.log("Pode entrar")
+//} else {
+    //console.log("Não pode entrar")
+
+//}
+//FUNÇÕES
+//function imprimirNome (nome, idade) {
+    //console.log(nome)
+    //console.log(idade)
+
+//}
+//imprimiNome ("Tiago Sestari")
+
+
+//function tirarMedia (numero_a, numero_b) {
+    //let soma = numero_a = numero_b
+    //let media = soma / 2
+    //console.log(media)
+    //return media
+//}
+
+//let mediarecebidadafuncao = tirarMedia(100, 50)
+//console.log(mediarecebidadafuncao)
+
+
+//criando uma funcao com arrow function
+//const tiraMedia_arrow = (numero_a, numero_b) => {
+    //let soma = numero_a + numero_b
+    // media = soma / 2
+    //console.log(media)
+    //return media
+//}
+
+
+
+
+
+
+
+
+
+
+
+
